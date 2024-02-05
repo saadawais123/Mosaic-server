281ecd6bf39a743599dee077c9dadf39a2692b9b
saadawais123 <saadawais122@gmail.com>
2024-02-05 23:34:43 +0500

    fixed topic service

--- src/controllers/topic.js
+++ src/controllers/topic.js
@@ -1,32 +1,20 @@
-const asyncHandler = require('express-async-handler');
-const { topicService } = require('../services');
-const { getResponse } = require('../helpers/response');
+const asyncHandler = require("express-async-handler");
+const { topicService } = require("../services");
+const { getResponse } = require("../helpers/response");
 
-require('dotenv').config();
-
+require("dotenv").config();
 
 const getAll = asyncHandler(async (req, res) => {
-    try {
-        const topics = await topicService.findAll();
-        if (topics) {
-            return getResponse(
-                res,
-                1,
-                'topics fetched succesfully',
-                200,
-                topics,
-                {},
-            );
-        }
-    } catch (error) {
-        return getResponse(res, 0, error?.message, 400, {}, {});
+  try {
+    const topics = await topicService.findAllTopics();
+    if (topics) {
+      return getResponse(res, 1, "topics fetched succesfully", 200, topics, {});
     }
+  } catch (error) {
+    return getResponse(res, 0, error?.message, 400, {}, {});
+  }
 });
 
-
-
 module.exports = {
-    getAll,
-
-
+  getAll,
 };
